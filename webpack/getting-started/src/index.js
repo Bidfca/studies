d0dafa736ae49fece656fc79e5f16312cd13c9ad
Bianca <bruninhakjurugat11@gmail.com>
2023-09-01 12:50:27 -0300

    Webpack basics

---
+++ webpack/getting-started/src/index.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+
+document.getElementById("button1").addEventListener("click", function () {
+  // Note: Webpack creates an IIFE (Immediately Invoked Function Expression), so "buttonClicked" won't be available globally when the button is clicked.
+  // function buttonClicked() {
+  const el = document.getElementById("header");
+  el.innerHTML = "Hey i have updated the code !";
+  const listItems = ["Apple", "orange", "Banana"];
+  const ul = document.getElementById("shoppingList");
+  _.forEach(listItems, function (item) {
+    const tempEl = document.createElement("li");
+    tempEl.innerHTML = item;
+    ul.appendChild(tempEl);
+  });
+  // }
+});
