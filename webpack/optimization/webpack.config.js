d2f739a803fedff83cc99c3068bdeca19d5c605e
Bianca <bruninhakjurugat11@gmail.com>
2023-09-24 18:37:52 -0300

    Remove dead CSS

--- webpack/optimization/webpack.config.js
+++ webpack/optimization/webpack.config.js
@@ -4,6 +4,12 @@ const CopyPlugin = require("copy-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const glob = require("glob");
+
+const purgePath = {
+  src: path.join(__dirname, "src"),
+};
 
 module.exports = {
   entry: {
@@ -63,6 +69,11 @@ module.exports = {
           context: "src",
         },
       ],
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${purgePath.src}/**/*`, { nodir: true }),
+      // Include the CSS unconditionally
+      // safelist: ["dummy-css"],
     }),
     new MiniCssExtractPlugin(),
     // new BundleAnalyzerPlugin({}),
